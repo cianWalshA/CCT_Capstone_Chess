54bdaad2cca5817fbcb9544f36605fde3b037209
cianWalshA <118617558+cianWalshA@users.noreply.github.com>
2023-07-26 19:58:58 +0100

    Removing previous analysis testing.
    Refining analysis of interest for testing

--- chessAnalysis.py
+++ chessAnalysis.py
@@ -3,28 +3,33 @@
 import sys
 import io
 import os
+import time
 import csv
 import chess
 import chess.pgn
 import stockfish
 import re
 import pandas as pd
+import numpy as np
 import seaborn as sns
 from pathlib import Path
 from datetime import datetime 
 
-stockfishEngine_Path = Path(rf"C:\Users\cianw\stockfish_15.1_win_x64_avx2\stockfish-windows-2022-x86-64-avx2.exe")
+stockfish_Path = Path(rf"C:\Users\cianw\Chess Engines\stockfish_15.1_win_x64_avx2\stockfish-windows-2022-x86-64-avx2.exe")
+lc0_Path = Path(rf"C:\Users\cianw\Chess Engines\lc0-v0.29.0-windows-gpu-nvidia-cuda\lc0.exe")
+ 
  
 csvFolder = r"E:\ChessData"
 pgnName = "lichess_db_standard_rated_2023-06_2000_5m"
-pgnOut = Path(rf"{csvFolder}\{pgnName}.csv")
+pgnIn = Path(rf"{csvFolder}\{pgnName}.csv")
+pgnOut = Path(rf"{csvFolder}\{pgnName}_details.tsv")
 
-lichessData = pd.read_csv(pgnOut, nrows=1000)
+lichessData = pd.read_csv(pgnIn, nrows=100000)
 lichessData['UTC_dateTime'] = pd.to_datetime(lichessData['UTCDate'] + ' ' + lichessData['UTCTime'])
 lichessData.describe()
 
 """
-Functions for Featur Extraction
+Functions for Feature Extraction
 """
 
 #Function to Extract Every Nth Word starting at position M, up to N*O words.
@@ -48,7 +53,128 @@ lichessData['whiteMoves_20'] = lichessData['Moves'].apply(lambda x: extract_nth_
 lichessData['blackMoves_20'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 3, 3, 20))
 
 
+moveTest = "1. e4 e5 2. Nf3 Nc6 3. Bc4 d6 4. h3 Nf6 5. Nc3 Be6 6. Be2 Qd7 7. O-O O-O-O 8. Re1 h6 9. d3 Bxh3 10. gxh3 Qxh3 11. Bf1 Qg4+ 12. Bg2 h5 13. Nh2 Qe6 14. Be3 g5 15. d4 exd4 16. Bxd4 h4 17. Nd5 Bg7 18. Nxf6 Bxf6 19. Bxf6 Qxf6 20. Bh3+ Kb8 21. c3 Rdg8 22. Ng4 Qe7 23. b4 Ne5 24. Re3 a6 25. Kg2 Rg6 26. Qd4 Rf8 27. Nxe5 dxe5 28. Qc4 g4 29. Rd1 gxh3+ 30. Kxh3 Qg5 31. Qf1 f5 32. exf5 Qxf5+ 33. Kh2 Qf4+ 34. Kh1 Qxe3 35. fxe3 Rxf1+ 36. Rxf1 Rg3 37. Re1 e4 38. Kh2 Kc8 39. Re2 Kd7 40. c4 Ke6 41. a4 Kf5 42. Kh1 Kg4 43. Kh2 Kf3 44. Re1 Kf2 45. Rd1 Kxe3 46. Rd7 Kf2 47. Rxc7 e3 48. Rf7+ Ke1 49. Rxb7 e2 50. Re7 Kf2 51. b5 axb5 52. cxb5 Re3 53. Rf7+ Ke1 54. b6 Kd2 55. Rd7+ Kc3 56. Rc7+ Kb4 57. Rc1 e1=Q 58. Rxe1 Rxe1"
 
+engineStockfish = chess.engine.SimpleEngine.popen_uci(stockfish_Path)
+engineLc0 = chess.engine.SimpleEngine.popen_uci(lc0_Path)
+
+
+
+def evaluateGame(games, engine1=None, engine2 = None):
+    if engine1==None:
+        print("Please input at least 1 engine for analysis")
+        return
+    gameMoves = chess.pgn.read_game(io.StringIO(games['Moves']))
+    gameMoves.headers
+
+    board = gameMoves.board()
+    evalList1 = []
+    depthList1 = []
+    evalList2 = []
+    depthList2 = []
+    evalDiff =[]
+    depthDiff =[]
+    for move in gameMoves.mainline_moves():
+        info1 = engine1.analyse(board, limit=chess.engine.Limit(time=0.01, depth=20))
+        score1 = info1['score'].white()
+        #check for mate and assign high value either way, maths says infinity but might not be suitable for analysis later
+        if score1.is_mate():
+            numericScore1 = float(10000) if score1.mate() > 0 else float(-10000)
+        else:
+            numericScore1 = score1.cp / 100.0
+        if engine2!=None:
+            info2 = engine2.analyse(board, limit=chess.engine.Limit(time=0.01, depth=20))
+            score2 = info2['score'].white()
+            #check for mate and assign high value either way, maths says infinity but might not be suitable for analysis later
+            if score2.is_mate():
+                numericScore2 = float(10000) if score2.mate() > 0 else float(-10000)
+            else:
+                numericScore2 = score2.cp / 100.0
+        
+        evalList1.append(numericScore1)
+        depthList1.append(info1['depth'])
+        evalList2.append(numericScore2)
+        depthList2.append(info2['depth'])
+        evalDiff.append(numericScore2-numericScore1)
+        depthDiff.append(info2['depth']-info1['depth'])
+        #print(f"Score: {info['score']},\tScore (white): " + f"{info['score'].white()},\tDepth: {info['depth']}")
+        board.push(move)
+    return evalList1, depthList1, evalList2, depthList2, evalDiff, depthDiff
+start_time = time.time()
+lichessData[['stockfish_eval','stockfish_depth', 
+             'Lc0_eval','Lc0_depth', 
+             'evalDiff','depthDiff']] = lichessData.apply(evaluateGame,
+                                                          engine1=engineStockfish,
+                                                          engine2=engineLc0, 
+                                                          axis=1, 
+                                                          result_type='expand')
+print("--- %s seconds ---" % (time.time() - start_time))                                                         
+
+
+
+
+lichessData.to_csv(pgnOut, sep="\t")
+
+lichessDate_read = pd.read_csv(pgnOut, sep="\t")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+engine.close()
+
+def evaluateGame(games):
+    #Create a new chess game from the PGN moves
+    game = chess.pgn.read_game(io.StringIO(games['Moves']))
+    
+    # Create a list to store the evaluations for each move
+    move_evaluations = []
+    
+    node = game
+    while node.variations:
+       next_node = node.variations[0]
+       eval_score = next_node.eval()
+       if eval_score is not None:
+           move_evaluations.append(eval_score.score())
+       else:
+           move_evaluations.append(0)  # Default value when evaluation is not available
+       node = next_node
+       
+ 
+    return move_evaluations   
+    
+    for move in game.mainline_moves():
+        board.push(move)
+        info = engine.analyse(board, chess.engine.Limit(time=0.1))
+        evaluation.append(info)
+
+    # Loop through each move in the game and get its evaluation
+    node = game
+    while node.variations:
+        next_node = node.variations[0]
+        eval_score = next_node.eval().score()
+        move_evaluations.append(eval_score)
+        node = next_node
+
+
+
+# Assume your DataFrame is named df
+lichessData['move_evaluations'] = lichessData.apply(evaluateGame, axis=1, result_type='expand')
+    
 
 
 
