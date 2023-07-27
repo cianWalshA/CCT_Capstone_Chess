bbe5a139f88353641357b5ee99572d0fcc99c1e3
cianWalshA <118617558+cianWalshA@users.noreply.github.com>
2023-07-27 20:57:04 +0100

    Rearranging programs
    Separating exploratory programs and renaming for suitability.
    ChessAnalysis -> chessEngineAnalysis
    chessExploration - New program

---
+++ ChessExploration.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 27 20:40:31 2023
+
+@author: cianw
+"""
+import sys
+import io
+import os
+import time
+import csv
+import chess
+import chess.pgn
+import stockfish
+import re
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from pathlib import Path
+from datetime import datetime 
+
+
+stockfish_Path = Path(rf"C:\Users\cianw\Chess Engines\stockfish_15.1_win_x64_avx2\stockfish-windows-2022-x86-64-avx2.exe")
+lc0_Path = Path(rf"C:\Users\cianw\Chess Engines\lc0-v0.29.0-windows-gpu-nvidia-cuda\lc0.exe")
+ 
+ 
+csvFolder = r"E:\ChessData"
+pgnName = "lichess_db_standard_rated_2023-06_2000_5m"
+pgnIn = Path(rf"{csvFolder}\{pgnName}.csv")
+pgnIn_EnglineAnalysis = Path(rf"{csvFolder}\{pgnName}_output.tsv")
+
+lichessData = pd.read_csv(pgnIn)
+lichessData['UTC_dateTime'] = pd.to_datetime(lichessData['UTCDate'] + ' ' + lichessData['UTCTime'])
+lichessData.describe()
+
+lichessData_EA = pd.read_csv(pgnIn_EnglineAnalysis)
+lichessData_EA['UTC_dateTime'] = pd.to_datetime(lichessData_EA['UTCDate'] + ' ' + lichessData_EA['UTCTime'])
+lichessData_EA.describe()
+
+"""
+SECTION 0 - 
+Functions
+"""
+#Function to Extract Every Nth Word of a string delimted by spaces starting at position M, up to N*O words.
+def extract_nth_words(text, M, N, O=None):
+    words = text.split()
+    if O is None:
+        endIndex = len(words)
+    else: 
+        endIndex = min(M-1+N*O, len(words))
+    result = [words[i] for i in range(M - 1, endIndex, N)]
+    return ' '.join(result)
+
+#Function to return every ith element of a list that was saved as a string
+def get_ith_element(lst, i):
+    res = lst.strip('][').split(', ')
+    if len(res) >= i+1:
+        return res[i]
+    else:
+        return None
+
+
+"""
+Section XYZ - Feature Extraction from Complete Set
+"""
+
+
+
+
+lichessData['moveNumbers'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 1, 3))
+lichessData['moveCount'] = lichessData['moveNumbers'].str.split().str.len()
+lichessData['whiteMoves_5'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 2, 3, 5))
+lichessData['blackMoves_5'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 3, 3, 5))
+lichessData['whiteMoves_10'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 2, 3, 10))
+lichessData['blackMoves_10'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 3, 3, 10))
+lichessData['whiteMoves_20'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 2, 3, 20))
+lichessData['blackMoves_20'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 3, 3, 20))
+
+
+
+
+#Where move count >= 5, >=10 return 10th and 20th values of rating respectively?
+
+"""
+SECTION - ABC
+Exploration of Engine Analysed Sub-Sample
+"""
+lichessData_EA['stockfish_eval_test'] = lichessData_EA['stockfish_eval'].apply(get_ith_element, i=5)
+
+lichessData_EA['sex'].value_counts()
+
+
+
+
+
+
+
--- chessEngineAnalysis.py
+++ chessEngineAnalysis.py
@@ -28,37 +28,10 @@ lichessData = pd.read_csv(pgnIn, nrows=100000)
 lichessData['UTC_dateTime'] = pd.to_datetime(lichessData['UTCDate'] + ' ' + lichessData['UTCTime'])
 lichessData.describe()
 
-"""
-Functions for Feature Extraction
-"""
-
-#Function to Extract Every Nth Word starting at position M, up to N*O words.
-def extract_nth_words(text, M, N, O=None):
-    words = text.split()
-    if O is None:
-        endIndex = len(words)
-    else: 
-        endIndex = min(M-1+N*O, len(words))
-    result = [words[i] for i in range(M - 1, endIndex, N)]
-    return ' '.join(result)
-
-
-lichessData['moveNumbers'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 1, 3))
-lichessData['moveCount'] = lichessData['moveNumbers'].str.split().str.len()
-lichessData['whiteMoves_5'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 2, 3, 5))
-lichessData['blackMoves_5'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 3, 3, 5))
-lichessData['whiteMoves_10'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 2, 3, 10))
-lichessData['blackMoves_10'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 3, 3, 10))
-lichessData['whiteMoves_20'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 2, 3, 20))
-lichessData['blackMoves_20'] = lichessData['Moves'].apply(lambda x: extract_nth_words(x, 3, 3, 20))
-
-
-moveTest = "1. e4 e5 2. Nf3 Nc6 3. Bc4 d6 4. h3 Nf6 5. Nc3 Be6 6. Be2 Qd7 7. O-O O-O-O 8. Re1 h6 9. d3 Bxh3 10. gxh3 Qxh3 11. Bf1 Qg4+ 12. Bg2 h5 13. Nh2 Qe6 14. Be3 g5 15. d4 exd4 16. Bxd4 h4 17. Nd5 Bg7 18. Nxf6 Bxf6 19. Bxf6 Qxf6 20. Bh3+ Kb8 21. c3 Rdg8 22. Ng4 Qe7 23. b4 Ne5 24. Re3 a6 25. Kg2 Rg6 26. Qd4 Rf8 27. Nxe5 dxe5 28. Qc4 g4 29. Rd1 gxh3+ 30. Kxh3 Qg5 31. Qf1 f5 32. exf5 Qxf5+ 33. Kh2 Qf4+ 34. Kh1 Qxe3 35. fxe3 Rxf1+ 36. Rxf1 Rg3 37. Re1 e4 38. Kh2 Kc8 39. Re2 Kd7 40. c4 Ke6 41. a4 Kf5 42. Kh1 Kg4 43. Kh2 Kf3 44. Re1 Kf2 45. Rd1 Kxe3 46. Rd7 Kf2 47. Rxc7 e3 48. Rf7+ Ke1 49. Rxb7 e2 50. Re7 Kf2 51. b5 axb5 52. cxb5 Re3 53. Rf7+ Ke1 54. b6 Kd2 55. Rd7+ Kc3 56. Rc7+ Kb4 57. Rc1 e1=Q 58. Rxe1 Rxe1"
+#moveTest = "1. e4 e5 2. Nf3 Nc6 3. Bc4 d6 4. h3 Nf6 5. Nc3 Be6 6. Be2 Qd7 7. O-O O-O-O 8. Re1 h6 9. d3 Bxh3 10. gxh3 Qxh3 11. Bf1 Qg4+ 12. Bg2 h5 13. Nh2 Qe6 14. Be3 g5 15. d4 exd4 16. Bxd4 h4 17. Nd5 Bg7 18. Nxf6 Bxf6 19. Bxf6 Qxf6 20. Bh3+ Kb8 21. c3 Rdg8 22. Ng4 Qe7 23. b4 Ne5 24. Re3 a6 25. Kg2 Rg6 26. Qd4 Rf8 27. Nxe5 dxe5 28. Qc4 g4 29. Rd1 gxh3+ 30. Kxh3 Qg5 31. Qf1 f5 32. exf5 Qxf5+ 33. Kh2 Qf4+ 34. Kh1 Qxe3 35. fxe3 Rxf1+ 36. Rxf1 Rg3 37. Re1 e4 38. Kh2 Kc8 39. Re2 Kd7 40. c4 Ke6 41. a4 Kf5 42. Kh1 Kg4 43. Kh2 Kf3 44. Re1 Kf2 45. Rd1 Kxe3 46. Rd7 Kf2 47. Rxc7 e3 48. Rf7+ Ke1 49. Rxb7 e2 50. Re7 Kf2 51. b5 axb5 52. cxb5 Re3 53. Rf7+ Ke1 54. b6 Kd2 55. Rd7+ Kc3 56. Rc7+ Kb4 57. Rc1 e1=Q 58. Rxe1 Rxe1"
 
 engineStockfish = chess.engine.SimpleEngine.popen_uci(stockfish_Path)
 engineLc0 = chess.engine.SimpleEngine.popen_uci(lc0_Path)
-
-
 
 def evaluateGame(games, engine1=None, engine2 = None):
     if engine1==None:
@@ -117,15 +90,6 @@ print("--- %s seconds ---" % (time.time() - start_time))
 
 lichessDate_read = pd.read_csv(pgnOut, sep="\t")
 
-def get_ith_element(lst, i):
-    res = lst.strip('][').split(', ')
-    if len(res) >= i+1:
-        return res[i]
-    else:
-        return None
-
-#Where move count >= 5, >=10 return 10th and 20th values of rating respectively?
-lichessDate_read['stockfish_eval_test'] = lichessDate_read['stockfish_eval'].apply(get_ith_element, i=5)
 
 
 
